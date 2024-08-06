a37d6a13bf3b097131167f3cd03e0364f9295df5
Will <mail@williamarnold.info>
2024-08-06 12:47:27 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by William Arnold on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
